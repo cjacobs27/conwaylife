8577d8237d875e876b095f17641820a7f6bc7635
Chelsey Jacobs <jacobs27c@gmail.com>
2023-09-26 13:10:45 +0100

    Render initial grid, handle start and quit

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import curses
 import time
 
-def main():
-    # live cell coordinates are stored in this defaultdict, dead ones are not
-    live_cells = Life(
-        {
-            (3, 3): 1
-        }
-    )
+def main(stdscr):
+    # Intro screen
+    # TODO: Add input for grid size here
+    stdscr.clear()
+    stdscr.addstr(8, 12, "Conway's Game of Life")
+    stdscr.addstr(10, 10, "Hit ENTER to start, Q to exit")
+    keypress = stdscr.getch()
 
-    game = render_grid(live_cells, (6, 6))
-    
-    print(game)
-    return game
+    if keypress == ord('\n'):
+        stdscr.clear()
+        # live cell coordinates are stored in this defaultdict, dead ones are not
+        live_cells = Life(
+            {
+                (2, 3): 1,
+                (3, 3): 1,
+                (3, 4): 1
+            }
+        )
+
+        grid = render_grid(live_cells, (10, 10))
+        
+        for key, value in grid.items():
+            value = '░' if value == 0 else '■'
+            stdscr.addstr(key[0], key[1], value)
+
+        # stdscr.nodelay(True)
+        # The starting coordinates for the viewport position
+        adjust_x, adjust_y = 0, 0
+
+        stdscr.refresh()
+        stdscr.getch()
+        
+        return grid
+    elif keypress == ord("q"):
+        exit(0)
+    #####
 
 def render_grid(live_cells, size):
     grid = {}
-    for x in range (0, size[0]):
-        for y in range (0, size[1]):
+    for x in range (1, size[0]):
+        for y in range (1, size[1]):
             grid[(x, y)] = live_cells[(x, y)]
-    
     return grid
-
-
 
 
 class Life(dict):
@@ -35,5 +56,5 @@ class Life(dict):
         return 0
 
 if __name__ == "__main__":
-    # curses.wrapper(main)
-    main()
+    curses.wrapper(main)
+    # main()
